c64fd5d308a180557f8ca71d8ef8471c7e62e619
tosch <akirahinoshiro@gmail.com>
2026-04-18 23:13:29 +0200

    code clean up

--- main.cpp
+++ main.cpp
@@ -5,12 +5,10 @@
 #include <vector>
 
 namespace stdx = std::experimental;
-constexpr std::size_t simd_size = stdx::simd<std::uint8_t>::size();
+constexpr std::size_t simd_size = stdx::simd<std::uint64_t>::size();
 
-template <typename T> void add_simd(const std::vector<T> &a, const std::vector<T> &b, std::vector<T> &c)
-{
-    for (std::size_t i = 0; i < a.size(); i += simd_size)
-    {
+template <typename T> void add_simd(const std::vector<T> &a, const std::vector<T> &b, std::vector<T> &c) {
+    for (std::size_t i = 0; i < a.size(); i += simd_size) {
         stdx::simd<T> va(a.data() + i, stdx::vector_aligned);
         stdx::simd<T> vb(b.data() + i, stdx::vector_aligned);
         stdx::simd<T> vc = va + vb;
@@ -18,49 +16,35 @@ template <typename T> void add_simd(const std::vector<T> &a, const std::vector<T
     }
 }
 
-template <typename T> void add_scalar(const std::vector<T> &a, const std::vector<T> &b, std::vector<T> &c)
-{
-    for (std::size_t i = 0; i < a.size(); ++i)
-    {
-        c[i] = a[i] + b[i];
+template <typename T> void add_scalar(const std::vector<T> &a, const std::vector<T> &b, std::vector<T> &c) {
+    for (std::size_t i = 0; i < a.size(); ++i) {
+        c.at(i) = a.at(i) + b.at(i);
     }
 }
 
-template <typename T> void Measure(std::size_t N)
-{
+template <typename T> void Measure(std::size_t N) {
     std::vector<T> a(N, 1);
     std::vector<T> b(N, 2);
     std::vector<T> c(N);
     auto start_simd = std::chrono::high_resolution_clock::now();
     add_simd(a, b, c);
     auto end_simd = std::chrono::high_resolution_clock::now();
-    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(end_simd - start_simd).count() << " microseconds" << std::endl;
-    for (auto val : c)
-    {
-        if (val != 3)
-        {
+    auto duration_simd = std::chrono::duration_cast<std::chrono::microseconds>(end_simd - start_simd).count();
+    std::cout << "Elapsed time: " << duration_simd << " microseconds" << std::endl;
+    for (auto val : c) {
+        if (val != 3) {
             std::cerr << "Error: expected 3, got " << static_cast<int>(val) << std::endl;
         }
     }
     auto start_scalar = std::chrono::high_resolution_clock::now();
     add_scalar(a, b, c);
     auto end_scalar = std::chrono::high_resolution_clock::now();
-    std::cout << "Elapsed time: " << std::chrono::duration_cast<std::chrono::microseconds>(end_scalar - start_scalar).count() << " microseconds" << std::endl;
-    for (auto val : c)
-    {
-        if (val != 3)
-        {
-            std::cerr << "Error: expected 3, got " << static_cast<int>(val) << std::endl;
-        }
-    }
-    std::cout << "performance improvement: "
-              << static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(end_scalar - start_scalar).count()) /
-                     std::chrono::duration_cast<std::chrono::microseconds>(end_simd - start_simd).count()
-              << "x" << std::endl;
+    auto duration_scalar = std::chrono::duration_cast<std::chrono::microseconds>(end_scalar - start_scalar).count();
+    std::cout << "Elapsed time: " << duration_scalar << " microseconds" << std::endl;
+    std::cout << "performance improvement: " << static_cast<double>(duration_scalar) / duration_simd << "x" << std::endl;
 }
 
-int main()
-{
+int main() {
     constexpr std::size_t N = simd_size * 100000000;
-    Measure<std::uint8_t>(N);
+    Measure<std::uint64_t>(N);
 }
